478c434f551e78048b06c569760b9a34b029b9ce
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2019-05-09 12:46:00 +1200

    Add

--- Assignment/src/Controller.js
+++ Assignment/src/Controller.js
@@ -2,8 +2,13 @@ class Controller {
 	static setup() {
 		let theNumberArray = new NumberArray()
 		
-		theNumberArray.addValues(0,10)
+		theNumberArray.addValues(83, 11.2)
+		theNumberArray.addValues(116, 9.3)
+		theNumberArray.addValues(186, 21.6)
+		theNumberArray.addValues(81, 6.9)
+		theNumberArray.addValues(114, 10.2)
 		
+		theNumberArray.calculateCorrelation()
 		
 		
 		return theNumberArray
--- Assignment/src/numberArray.js
+++ Assignment/src/numberArray.js
@@ -10,6 +10,42 @@ class NumberArray {
 	
 	}
 	
+	calculateCorrelation() {
+		
+		var n = this.allMyXValues.length
+		
+		var xTotal = 0
+		for (var i = 0; i < n; i++) { 
+			xTotal += Number(this.allMyXValues[i])
+		} 
+		console.log(xTotal)
+		var yTotal = 0
+		for (var i = 0; i < n; i++) { 
+			yTotal += Number(this.allMyYValues[i])
+		}		
+		console.log(yTotal)
+		var xSquared = 0
+		for (var i = 0; i < n; i++) { 
+			xSquared += Math.pow(Number(this.allMyXValues[i]),2)
+		}
+		console.log(xSquared)
+		var ySquared = 0
+		for (var i = 0; i < n; i++) { 
+			ySquared += Math.pow(Number(this.allMyYValues[i]),2)
+		}
+		console.log(ySquared)		
+		var xTimesy = 0
+		for (var i = 0; i < n; i++) {
+			xTimesy += ((Number(this.allMyXValues[i]))*(Number(this.allMyYValues[i])))
+		}
+		console.log(xTimesy)
+		
+		var finalValue = ((n*xTimesy)-(xTotal*yTotal)) / Math.sqrt((n*xSquared)-(Math.pow(xTotal,2)) * ((n*ySquared)-(Math.pow(yTotal,2))))
+		console.log(finalValue)
+		
+		return finalValue
+	}
+	
 	
 
 
