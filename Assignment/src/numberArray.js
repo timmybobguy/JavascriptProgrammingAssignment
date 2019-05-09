0f926fa2e6118eb77655c028af0c9183033cf306
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2019-05-09 17:20:37 +1200

    Fixes

--- Assignment/src/numberArray.js
+++ Assignment/src/numberArray.js
@@ -10,35 +10,56 @@ class NumberArray {
 
 	}
 
-	calculateCorrelation() {
-
-		var n = this.allMyXValues.length
-
-		var xTotal = 0
+	xTotal() {
+		xTotal = 0
 		for (var i = 0; i < n; i++) {
 			xTotal += Number(this.allMyXValues[i])
 		}
-		var yTotal = 0
+		return xTotal
+	}
+
+	yTotal() {
+		yTotal = 0
 		for (var i = 0; i < n; i++) {
 			yTotal += Number(this.allMyYValues[i])
 		}
-		var xSquared = 0
+		return yTotal
+	}
+
+	n() {
+		return this.allMyXValues.length
+	}
+
+	xSquared() {
+		xSquared = 0
 		for (var i = 0; i < n; i++) {
 			xSquared += Math.pow(Number(this.allMyXValues[i]),2)
 		}
-		var ySquared = 0
+		return xSquared
+	}
+
+	ySquared() {
+		ySquared = 0
 		for (var i = 0; i < n; i++) {
 			ySquared += Math.pow(Number(this.allMyYValues[i]),2)
 		}
-		var xTimesy = 0
+		return ySquared
+	}
+
+	xTimesy() {
+		xTimesy = 0
 		for (var i = 0; i < n; i++) {
 			xTimesy += ((Number(this.allMyXValues[i]))*(Number(this.allMyYValues[i])))
 		}
+		return xTimesy
+	}
+	calculateCorrelation() {
+		return ((n()*xTimesy())-(xTotal()*yTotal())) / Math.sqrt(((n()*xSquared())-(Math.pow(xTotal(),2))) * ((n()*ySquared())-(Math.pow(yTotal(),2))))
+	}
 
-		var finalValue = ((n*xTimesy)-(xTotal*yTotal)) / Math.sqrt(((n*xSquared)-(Math.pow(xTotal,2))) * ((n*ySquared)-(Math.pow(yTotal,2))))
-		console.log(finalValue)
-
-		return finalValue
+	calculateLinearRegression() {
+		var xAve = xTotal / n
+		console.log(xAve)
 	}
 
 
